429b74fc1ad3b88e34c2a0e5ba36ba4b81bae4b2
Gözde Altuntaş <altuntasgozde@gmail.com>
2021-04-15 14:40:10 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import "./App.css";
 
 function App() {
+
+  const [billAmount, setBillAmount] = useState()
+
+  const [person, setPerson] = useState()
+
+  const [percent, setPercent] = useState()
+
+  const [total, setTotal] = useState()
+
+  const ChangeBillValue = (e) => {
+    setBillAmount(e.target.value)
+  }
+
+  const ChangePerson = (e) => {
+    setPerson(e.target.value)
+  }
+
+  const ChangePercent = (e) => {
+    setPercent(e.target.value)
+  }
+
+//(billAmount.value*(30/100))/people.value
+
+  const Calculate = () => {
+    
+    setTotal((billAmount*(percent/100))/person)
+ 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="billAmount">
+        <label>How much your bill?</label>
+        <input value={billAmount} onChange={ChangeBillValue}></input>
+      </div>
+      <div className="percent">
+        <label>How was your service? </label>
+        <select value={percent} onChange={ChangePercent}>
+          <option disabled selected >Choose an option</option>
+          <option value="10">%10 - Bad</option>
+          <option value="20">%20 - It was OK</option>
+          <option value="30">%30 - Good</option>
+          <option value="40">%40 - Outstanding</option>
+        </select>
+
+      </div>
+      <div className="person">
+        <label>How many people shairing the bill?</label>
+        <input value={person} onChange={ChangePerson}></input>
+      </div>
+      <div className="buttonArea">
+        <button onClick={Calculate}>Calculate</button>
+        <h1>{total}</h1>
+      </div>
     </div>
   );
 }
