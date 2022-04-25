4e8243c34c7c99569964c91e33b742100a023a92
Gözde <altuntasgozde@gmail.com>
2022-04-25 15:57:35 +0300

    Style changes

--- src/App.js
+++ src/App.js
@@ -5,41 +5,32 @@ function App() {
 
   const [billAmount, setBillAmount] = useState()
 
-  const [person, setPerson] = useState()
+  const [person, setPerson] = useState();
 
-  const [percent, setPercent] = useState()
+  const [percent, setPercent] = useState();
 
-  const [total, setTotal] = useState()
-
-  const ChangeBillValue = (e) => {
-    setBillAmount(e.target.value)
-  }
-
-  const ChangePerson = (e) => {
-    setPerson(e.target.value)
-  }
-
-  const ChangePercent = (e) => {
-    setPercent(e.target.value)
-  }
+  const [total, setTotal] = useState();
 
 
   const Calculate = () => {
-    
     setTotal((billAmount*(percent/100))/person)
- 
   }
 
   return (
     <div className="App">
-      <h1>TIP CALCULATOR</h1>
+      <div className='main'>
+      <h2>TIP CALCULATOR</h2>
       <div className="billAmount">
         <label>How much your bill?</label>
-        <input value={billAmount} onChange={ChangeBillValue}></input>
+        <input value={billAmount} onChange={(e) => setBillAmount(e.target.value)}></input>
+      </div>
+      <div className="person">
+        <label>How many people shairing the bill?</label>
+        <input value={person} onChange={(e) => setPerson(e.target.value)}></input>
       </div>
       <div className="percent">
         <label>How was your service? </label>
-        <select value={percent} onChange={ChangePercent}>
+        <select value={percent} onChange={(e) => setPercent(e.target.value)}>
           <option disabled selected >Choose an option</option>
           <option value="10">%10 - Bad</option>
           <option value="20">%20 - It was OK</option>
@@ -48,14 +39,13 @@ function App() {
         </select>
 
       </div>
-      <div className="person">
-        <label>How many people shairing the bill?</label>
-        <input value={person} onChange={ChangePerson}></input>
-      </div>
+      
       <div className="buttonArea">
         <button onClick={Calculate}>Calculate</button>
-        <h1>{total}</h1>
+        <h2>{total}</h2>
       </div>
+      </div>
+ 
     </div>
   );
 }
